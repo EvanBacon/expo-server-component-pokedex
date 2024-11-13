99f56b40695395e42a879c74fde121c78d086ff4
evanbacon <bacon@expo.io>
2024-11-13 12:29:09 -0600

    upgrade to SDK 52

--- src/app/info.tsx
+++ src/app/info.tsx
@@ -1,12 +1,5 @@
 import InfoPage from "@/components/info-page";
 
 export default function InfoRoute() {
-  return (
-    <InfoPage
-      dom={{
-        contentInsetAdjustmentBehavior: "automatic",
-        automaticallyAdjustsScrollIndicatorInsets: true,
-      }}
-    />
-  );
+  return <InfoPage />;
 }
